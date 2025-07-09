38494194364960bfe1eb50aae0895c72a559c5c6
Aadi-0411 <aadityapatil2004@gmail.com>
2025-07-09 10:12:20 +0530

    importing from sections into app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,12 @@
+import Nav from './sections/Nav';
 import Hero from './sections/Hero';
+import PopularProducts from './sections/PopularProducts';
+import SuperQuality from './sections/SuperQuality';
+import Services from './sections/Services';
+import SpecialOffer from './sections/SpecialOffer';
+import CustomerReviews from './sections/CustomerReviews';
+import Subscribe from './sections/Subscribe';
+import Footer from './sections/Footer';
 
 const App = () => (
   <main className="relative">
